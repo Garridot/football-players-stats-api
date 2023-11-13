67a3a44811950b7f94e02f53a62464e364526264
Garridot <garridot210@gmail.com>
2023-11-13 16:21:21 -0300

    Fixed 'ALLOWED_HOSTS' in production.py; it should be a list of strings rather than a string with multiple hosts separated by commas.

--- football_players_stats_api/settings/production.py
+++ football_players_stats_api/settings/production.py
@@ -1,6 +1,10 @@
 from .base import *
-DEBUG = env('DEBUG_PRODUCTION')
-ALLOWED_HOSTS = [env('ALLOWED_HOSTS_PRODUCTION')]
+
+DEBUG=env("DEBUG_PRODUCTION",default=False,)
+# Retrieve the value of ALLOWED_HOSTS_PRODUCTION from .env file
+ALLOWED_HOSTS_PRODUCTION = env('ALLOWED_HOSTS_PRODUCTION', default='', cast=lambda v: [s.strip() for s in v.split(',')])
+# Set ALLOWED_HOSTS using the retrieved value
+ALLOWED_HOSTS = ALLOWED_HOSTS_PRODUCTION
 SECRET_KEY = env('SECRET_KEY')
 
 
