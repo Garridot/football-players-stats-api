206b75b300858c94bb209bc6cb300b2f88462690
Tomas Garrido <garridot210@gmail.com>
2025-11-07 20:34:11 -0300

    update whitenoise

--- project_package/settings/base.py
+++ project_package/settings/base.py
@@ -39,6 +39,7 @@ LOCAL_APPS = [
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
