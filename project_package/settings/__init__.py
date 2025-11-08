8347134d5a1cc185991b953d977d6603b1931951
Tomas Garrido <garridot210@gmail.com>
2025-11-08 17:11:35 -0300

    change the variables initialization in the 'settings/base.py' file.

--- project_package/settings/__init__.py
+++ project_package/settings/__init__.py
@@ -1,5 +1,5 @@
 from .base import *
-from .jwt import *
+
 from .static import *
 
 import os
--- project_package/settings/base.py
+++ project_package/settings/base.py
@@ -81,3 +81,22 @@ USE_TZ = True
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+from datetime import timedelta
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+    'SLIDING_TOKEN_LIFETIME': timedelta(days=30),
+}
