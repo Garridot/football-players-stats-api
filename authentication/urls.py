e220d8596dc60e6ec1b9ba83a9477e2d7b0c177c
Tomas Garrido <garridot210@gmail.com>
2025-11-08 21:55:06 -0300

    .

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import RegisterView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+app_name = 'authentication'
 urlpatterns = [  
     path('register/', RegisterView.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
--- project_package/urls.py
+++ project_package/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),    
-    path('auth/', include(('authentication.urls', 'authentication'), namespace='authentication')),
+    path('auth/', include('authentication.urls', namespace='authentication')),
     path('', include('data_manager.urls'))
 ]
