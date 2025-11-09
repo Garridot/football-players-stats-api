2eb5dda4152b1a324ace54b6212cd25cde5edc25
Tomas Garrido <81374799+Garridot@users.noreply.github.com>
2025-11-08 22:54:01 -0300

    Add api_root path to urlpatterns

--- project_package/urls.py
+++ project_package/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),    
     path('auth/', include('authentication.urls', namespace='authentication')),
+    path('', api_root, name='api-root'),
     path('', include('data_manager.urls'))
 ]
